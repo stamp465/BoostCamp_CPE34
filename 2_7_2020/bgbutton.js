d06c362ba8e809e10852111b0480194f43202f21
Shasn <40135787+stamp465@users.noreply.github.com>
2020-07-03 14:04:17 +0700

    Add files via upload

--- 2_7_2020/bgbutton.js
+++ 2_7_2020/bgbutton.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
-    $("#bg").click(function(){
-                if ($("#bg").prop('checked')) {
-                              alert('Change background color to light blue!');
-                                          $("body").css({"background-color": "lightblue"});
-                                                    } else {
-                                                                  $("body").css({"background-color": "white"});
-                                                                            }
-                                                                                      });
-          });
+$(document).ready(function () {
+    $("#bg").click(function () {
+        if ($("#bg").prop('checked')) {
+            alert('Change background color to coral!');
+            $("body").css({ "background-color": "coral" });
+        } else {
+            $("body").css({ "background-color": "white" });
+        }
+    });
+});
 
